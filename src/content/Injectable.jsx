b597c80ac8b2bcf9cb8c60ff7b37aee6432817fd
oklandon <oklandon@gmail.com>
2017-06-22 19:11:55 -0600

    vin2hoob extension

---
+++ src/content/Injectable.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import styled, { keyframes } from 'styled-components';
+
+
+class Injectable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            windowHeight: 590,
+        };
+    }
+
+    componentWillMount() {
+        this.state = {
+            windowHeight: window.innerHeight,
+        };
+    }
+
+    render() {
+        const kf = keyframes`
+            to {
+                top: 0;
+                right: 0;
+                opacity: 1;
+                transform: scale(${this.props.popupOn ? 1 : 0});
+            }
+        `;
+
+        const StyledDiv = styled.div`
+            opacity: 0;
+            position: fixed;
+            transform-origin: 100% 0;
+            transform: scale(0);
+            top: -500px;
+            z-index: 2147483647;
+            animation: ${kf} 0.7s ease-out forwards;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 12px;
+            border-left: 1px solid #f2f2f2;
+            background-color: #F6F6F6;
+        `;
+
+        return (
+            <StyledDiv id='JID-injectable'>
+                <iframe
+                    name            = 'ext'
+                    src             = { chrome.extension.getURL('src/action/popup/popup.html') }
+                    height          = { this.state.windowHeight }
+                    width           = '420'
+                    frameBorder     = '0'
+                    allowFullScreen = 'true'
+                >
+                </iframe>
+            </StyledDiv>
+        );
+    }
+}
+
+export default connect(state => ({ popupOn: state.popupOn }))(Injectable);
---
+++ src/content/content2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'react-chrome-redux';
+import Injectable from './Injectable';
+
+
+const proxyStore = new Store({ portName: 'JIDPort' });
+
+if ( ! document.querySelector('#reactApp')) {
+    let anchor = document.createElement('div');
+    anchor.id = 'reactApp';
+
+    document.body.insertBefore(anchor, document.body.childNodes[0]);
+
+    render(
+        <Provider store={proxyStore}><Injectable /></Provider>,
+        document.getElementById('reactApp')
+    );
+}
